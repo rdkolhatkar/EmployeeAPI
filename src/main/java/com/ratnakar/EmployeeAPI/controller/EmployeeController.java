61f086bc6eed3568cddf68d1e4ab8897969b54af
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-08-04 13:55:54 +0530

    Get and Delete API development

--- src/main/java/com/ratnakar/EmployeeAPI/controller/EmployeeController.java
+++ src/main/java/com/ratnakar/EmployeeAPI/controller/EmployeeController.java
@@ -16,7 +16,7 @@ public class EmployeeController {
 
     @Autowired
     EmployeeResponseSetup employeeResponseSetup;
-     @Autowired
+    @Autowired
     EmployeeService employeeService;
 
     @Autowired
